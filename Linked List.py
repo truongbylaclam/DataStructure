93637d8426e592432ee2c0504750e52f09bb0f1d
truongbylaclam <truongbylaclam@gmail.com>
2021-11-09 17:16:19 -0800

    commit

--- Linked List.py
+++ Linked List.py
@@ -4,7 +4,7 @@
 class Node(object):
     def __init__(self, val):
         self.val = val
-        self.next = NULL
+        self.next = None
     def getVal(self):
         return self.val
     def setVal(self, val):
@@ -18,6 +18,34 @@ class Node(object):
 class LinkedList(object):
     def __init__(self, head= None):
         self.head = head
-        self.count = 0
+        self.count = 0  
+    def getCount(self):
+        return self.count
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.setNext(self.head)
+        self.head = new_node
+        self.count += 1
+        print("Insert completed")
+    def find(self, val):
+        item = self.head
+        while (item != None):
+            if (item.getVal() == val):
+                return item
+            else:
+                item = item.getNext()
+        return None
+    def deleteAt(self, idx):
+        if idx > self.count - 1:
+            return
+        if idx == 0:
+            self.head = self.head.getNext();
+        else:
+            tempidx = 0
+            return
 
+list = LinkedList()
+list.insert(10)
+
+print(list.getCount())
 
